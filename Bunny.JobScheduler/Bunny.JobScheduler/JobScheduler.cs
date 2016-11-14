3cfede20e21a9ecc71a567df3faf9ce6c11077c3
Bunny <ggtech.89@gmail.com>
2016-11-15 00:05:58 +0530

    Added Project High level Architecture

---
+++ Bunny.JobScheduler/Bunny.JobScheduler/JobScheduler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bunny.JobScheduler.Interfaces;
+
+namespace Bunny.JobScheduler
+{
+    public class JobScheduler : IJobScheduler
+    {
+        private bool isStarted;
+        private bool isDisposed;
+
+        public void Start()
+        {
+            if (isStarted)
+            {
+                throw new InvalidOperationException("The Job Scheduler is already running.");
+            }
+        }
+
+        public void Stop()
+        {
+            
+        }
+
+        public string AddAsynchronousJob()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            if(!isDisposed)
+            {
+                this.Stop();
+                this.isDisposed = true;
+            }
+        }
+    }
+}
